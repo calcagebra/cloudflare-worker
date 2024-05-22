af30162e98cb92e8dc9a96e5b2d33a14374599f6
Megatank58 <tanumegatank@gmail.com>
2024-05-22 04:54:29 +0000

    feat: debug option

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,15 @@ use std::sync::Arc;
 use lexer::Lexer;
 
 use crate::{interpreter::Interpreter, parser::Parser};
+use serde::Deserialize;
 
 use worker::*;
+
+#[derive(Debug, Deserialize)]
+pub struct RequestBody {
+    code: String,
+    debug: bool
+}
 
 #[event(fetch)]
 async fn main(mut req: Request, _env: Env, _ctx: Context) -> Result<Response> {
@@ -21,18 +28,27 @@ async fn main(mut req: Request, _env: Env, _ctx: Context) -> Result<Response> {
         .with_origins(["*"])
         .with_allowed_headers(["*"]);
 
-    let res = Response::ok(run(req.text().await.unwrap()));
+    let json = req.json::<RequestBody>().await.unwrap();
+
+    let res = Response::ok(run(json.code, json.debug));
 
     res?.with_cors(&cors)
 }
 
-pub fn run(contents: String) -> String {
+pub fn run(contents: String, debug: bool) -> String {
     std::io::set_output_capture(Some(Default::default()));
-    let tokens = Lexer::new(&contents).tokens();
-    let ast = Parser::new(tokens).ast();
-    Interpreter::new().run(ast);
-    let captured = std::io::set_output_capture(None);
 
+    let tokens = Lexer::new(&contents).tokens();
+    if debug {
+        println!("{tokens:?}")
+    }
+    let ast = Parser::new(tokens).ast();
+    if debug {
+        println!("{ast:?}")
+    }
+    Interpreter::new().run(ast);
+
+    let captured = std::io::set_output_capture(None);
     let captured = captured.unwrap();
     let captured = Arc::try_unwrap(captured).unwrap();
     let captured = captured.into_inner().unwrap();
